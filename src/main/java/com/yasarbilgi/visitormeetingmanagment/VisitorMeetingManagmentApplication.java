380006be4809f11cdc4c7e18b1a3f7130e516637
Elif Nur Beycan <elif.bycn35@gmail.com>
2026-07-10 11:39:11 +0300

    Initial project structure

---
+++ src/main/java/com/yasarbilgi/visitormeetingmanagment/VisitorMeetingManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.yasarbilgi.visitormeetingmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorMeetingManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisitorMeetingManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yasarbilgi/visitormeetingmanagment/VisitorMeetingManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasarbilgi.visitormeetingmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorMeetingManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
