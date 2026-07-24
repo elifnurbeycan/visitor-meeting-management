553e8a3b972d6ced011b812eec6056f1fd06a19b
Altan Koc <altankocdev@gmail.com>
2026-07-24 10:24:30 +0300

    feat: add tenant path guard interceptor for IDOR protection

--- src/main/java/com/yasarbilgi/visitormeetingmanagment/common/config/WebMvcConfig.java
+++ src/main/java/com/yasarbilgi/visitormeetingmanagment/common/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.yasarbilgi.visitormeetingmanagment.common.config;
 
 import com.yasarbilgi.visitormeetingmanagment.security.interceptor.TenantFilterInterceptor;
+import com.yasarbilgi.visitormeetingmanagment.security.interceptor.TenantPathGuardInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
@@ -12,9 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final TenantFilterInterceptor tenantFilterInterceptor;
+    private final TenantPathGuardInterceptor tenantPathGuardInterceptor;
 
     @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(tenantPathGuardInterceptor).order(50);
         registry.addInterceptor(tenantFilterInterceptor).order(100);
+
     }
 }
